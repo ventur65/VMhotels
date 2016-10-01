f4bcd56d5a84c83f97a2a3358db59a8ea691a0b5
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-10-01 19:46:53 +0200

    Inserita modifica dati utente

---
+++ portal/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+class UserForm(ModelForm):
+    class Meta:
+    	model = User
+    	fields = ['username', 'first_name', 'last_name', 'email']
