30b6f5cb8b124d75011e21d6da7ab418a0d1d344
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-10-01 19:58:56 +0200

    Aggiunta modifica dati utente 2

--- portal/urls.py
+++ portal/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
 	#ex: /portal/
 	url(r'^$', views.portal_welcome, name='portal_welcome'),
+	url(r'^change/$', views.upload, name='update_data'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.contrib.auth.models import User
+from portal.forms import *
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 @login_required
@@ -10,3 +13,24 @@ def portal_welcome(request):
 	(template index.html). Otherwise, take them to the login page.
 	"""
 	return render(request, 'portal/index.html', {'request': request})
+	
+@login_required       
+def upload(request):
+    if request.method == 'POST':
+    	b = User.objects.get(pk=request.user.id)
+    	form = UserForm(request.POST, instance=b)
+    	if form.is_valid():
+    		if request.user.is_authenticated():
+			form.save()
+			return HttpResponse('Dati aggiornati')
+	else:
+		return HttpResponse('User non autenticato')
+    else:
+    	form_data = {
+    		'username':request.user.username,
+    		'first_name':request.user.first_name,
+    		'last_name':request.user.last_name,
+    		'email':request.user.email,
+    	}
+    	form = UserForm(data=form_data)
+    	return render(request, 'portal/upload.html', {'form' : form})	
