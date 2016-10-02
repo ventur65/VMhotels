1442d9b00ffb831cf2fbc8c715475d2dc9e37b79
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-10-02 17:13:36 +0200

    Aggiunta modifica dettagli Hotel e Room

--- hotels/urls.py
+++ hotels/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
 	url(r'^createhotel/$', views.create_hotel, name = 'create_hotel'),
 	#ex: /hotels/5/createroom/
 	url(r'^(?P<hotel_id>[0-9]+)/createroom/$', views.create_room, name = 'create_room'),
+	#ex: /hotels/5/edithotel/
+	url(r'^(?P<hotel_id>[0-9]+)/edithotel/$', views.edit_hotel, name='edit_hotel'),
+	#ex: /hotels/1/2/editroom
+	url(r'^(?P<hotel_id>[0-9]+)/(?P<room_id>[0-9]+)/editroom/$', views.edit_room, name='edit_room'),
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -46,6 +46,74 @@ def create_room(request, hotel_id):
 	else: #caso GET
 		form = RoomForm()
 	return render(request, 'hotels/insertroom.html', {'form': form,})
+
+@login_required()	
+def edit_hotel(request, hotel_id):
+    if request.method == 'POST':
+    
+    	h = get_object_or_404(Hotel, pk=hotel_id)
+    	
+    	form = HotelForm(request.POST, request.FILES, instance = h)
+    	
+    	if form.is_valid():
+    		if request.user == h.user:
+			r = form.save(commit = False)
+			r.user = request.user
+			r.image = request.FILES['image']
+			r.save()
+			return HttpResponseRedirect("/hotels/"+str(h.id))
+	else:
+		return HttpResponse('Dati non validi')
+    else:
+    
+    	h = get_object_or_404(Hotel, pk=hotel_id)
+    	
+    	form_data = {
+    		'name':h.name,
+    		'city':h.city,
+    		'address':h.address,
+    		'description':h.description,
+    		'email':h.email,
+    		'tel':h.tel,
+    		'image':h.image,
+    	}
+    	form = HotelForm(data=form_data)
+    	return render(request, 'hotels/edithotel.html', {'form' : form}) 
+    	
+@login_required()	
+def edit_room(request, hotel_id, room_id):
+    if request.method == 'POST':
+    
+    	h = get_object_or_404(Hotel, pk=hotel_id)
+    	r = get_object_or_404(Room, pk=room_id)
+    	
+    	form = RoomForm(request.POST, request.FILES, instance = r)
+    	
+    	if form.is_valid():
+    		if request.user == h.user:
+    			if h.pk == r.hotel.id:
+				ro = form.save(commit = False)
+				ro.hotel = h
+				ro.image = request.FILES['image']
+				ro.save()
+				return HttpResponseRedirect("/hotels/"+str(h.id)+"/"+str(r.id))
+			else:
+				return HttpResponse ('Room non appartenente all hotel')
+	else:
+		return HttpResponse('Dati non validi')
+    else:
+    
+    	r = get_object_or_404(Room, pk=room_id)
+    	
+    	form_data = {
+    		'number':r.number,
+    		'beds':r.beds,
+    		'description':r.description,
+    		'cost':r.cost,
+    		'image':r.image,
+    	}
+    	form = RoomForm(data=form_data)
+    	return render(request, 'hotels/editroom.html', {'form' : form})
 			
 			
 			
