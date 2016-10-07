41e31924ff6836fb4cff0d2f3e21173a4f16cd6f
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-10-07 11:00:28 +0200

    Aggiunta modifica e cancellazione prenotazione

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^(?P<hotel_id>[0-9]+)/(?P<room_id>[0-9]+)/add/$', views.add_reservation, name="add_reservation"),
 	url(r'^(?P<reservation_id>[0-9]+)/$', views.reservation_detail, name="reservation_detail"),
+	url(r'^(?P<reservation_id>[0-9]+)/editres$', views.edit_reservation, name="edit_reservation"),
+	url(r'^(?P<reservation_id>[0-9]+)/delres$', views.delete_reservation, name="delete_reservation"),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -11,6 +11,8 @@ from django.views import generic
 ##NON E' UNA VIEW.
 def check_res(newres, room):
 	for res in Reservation.objects.filter(room = room):
+		if (res == newres):
+			continue
 		if (newres.idate >= res.idate) and (newres.idate <= res.fdate):
 			return False
 		elif (newres.fdate >= res.idate) and (newres.fdate <= res.fdate):
@@ -51,6 +53,40 @@ def reservation_detail(request, reservation_id):
 		return render(request, 'reservation/reservation_detail.html', {'reservation': res})
 	return HttpResponse("NON SEI L'UTENTE CHE HA EFFETTUATO QUESTA PRENOTAZIONE")
 	
+@login_required
+def edit_reservation(request, reservation_id):
+	res = get_object_or_404(Reservation, pk=reservation_id)
+	r = get_object_or_404(Room, pk=res.room.pk)
+	h = get_object_or_404(Hotel, pk=r.hotel.pk)
+	if res.user != request.user: ##Se l'utente della sessione non e' il titolare della prenotazione
+		return HttpResponseForbidden("You can't edit a reservation of a reservation not yours")
+	if 'Ok' in request.POST:
+		form = ReservationForm(request.POST, instance = res)
+		if form.is_valid():
+			form.save(commit=False)
+			if check_res(res, r):
+				res.user = request.user
+				res.room = r
+				res.save()
+				r.save()
+				return HttpResponseRedirect(reverse('reservation:reservation_detail', args=(res.id,)))
+			else:
+				return HttpResponse("Stanza gia prenotata in quel periodo")
+	elif request.method == 'GET': ##caso GET
+		form = ReservationForm(instance = res)
+		return render(request, 'reservation/editreservation.html', {'form': form, 'hotel': h, 'room': r, 'reservation':res,})
+	return HttpResponse("This reservation does not exists for this user")
 	
-	
-	
+@login_required
+def delete_reservation(request, reservation_id):
+	res = get_object_or_404(Reservation, pk=reservation_id)
+	r = get_object_or_404(Room, pk=res.room.pk)
+	h = get_object_or_404(Hotel, pk=r.hotel.pk)
+	if res.user != request.user: ##Se l'utente della sessione non e' il titolare della prenotazione
+		return HttpResponseForbidden("You can't edit a reservation of a reservation not yours")
+	if 'Ok' in request.POST:
+		Reservation.objects.filter(id=res.id).delete()
+		return HttpResponse("Prenotazione cancellata con successo")
+	else:
+		return render(request, 'reservation/deletereservation.html', {'hotel': h, 'room': r, 'reservation':res,})
+	return HttpResponse("This reservation does not exists for this user")
