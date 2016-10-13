b2effe15aee70963dc723abb255c895093b068d6
ventur65 <ventur65@hotmail.it>
2016-10-13 18:51:38 +0200

    Sistemata la Ricerca

--- VMhotels/views.py
+++ VMhotels/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from hotels.models import Hotel, Room
 from review.models import Review
 from datetime import datetime
-from django.db.models import Avg
+from django.db.models import Avg, Min, Max
 
 def main_page(request):
 	if request.method == 'GET':
@@ -15,15 +15,13 @@ def search_results(request):
 	check_in = datetime.strptime(request.POST['checkin'], "%Y/%m/%d")
 	check_out = datetime.strptime(request.POST['checkout'], "%Y/%m/%d")
 	days = check_out - check_in
+	rl = dict()
 	found_entries = Hotel.objects.filter(city=dest, room__beds=beds).distinct()
-	rl = []
 	if 'rate' in request.POST:
 		found_entries = found_entries.annotate(average=Avg('review__rate')).order_by('-average')
 	else:
 		#found_entries = found_entries.annotate(average=Avg('room__cost')).order_by('average')
 		found_entries = found_entries.order_by('room__cost')
 	for h in found_entries:
-		for r in h.room_set.filter(beds=beds).order_by('cost'):
-			rl.append(r)
-	print rl
+		rl[h] = h.room_set.filter(beds=beds).aggregate(costmin=Min('cost'), costmax=Max('cost'))
 	return render(request, 'search.html', {'found_entries': found_entries, 'days': days.days, 'rl': rl})
