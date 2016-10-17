c57913fe5f404591120cf6da4a1ce56e2d17e576
ventur65 <ventur65@hotmail.it>
2016-10-17 16:34:35 +0200

    Controllo Date Form Reservation

--- reservation/forms.py
+++ reservation/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from hotels.models import Hotel, Room
 from .models import Reservation
 from bootstrap3_datetime.widgets import DateTimePicker
+from datetime import date
 
 class ReservationForm(forms.ModelForm):
 	class Meta:
@@ -13,4 +14,20 @@ class ReservationForm(forms.ModelForm):
 								     "pickTime": False})
 		self.fields['fdate'].widget = DateTimePicker(options={"format": "YYYY-MM-DD",
 								     "pickTime": False})
-	
+	def clean(self):
+		cleaned_data = super(ReservationForm, self).clean()
+		idate = cleaned_data.get('idate')
+		fdate = cleaned_data.get('fdate')
+		
+		if idate and fdate and (idate >= fdate):
+			msg = "Initial Date must be earlier than Final Date"
+			self.add_error('idate', msg)
+			self.add_error('fdate', msg)
+		
+		if idate and (idate < date.today()):
+			msg = "Date in the past"
+			self.add_error('idate', msg)
+		
+		if fdate and (fdate < date.today()):
+			msg = "Date in the past"
+			self.add_error('fdate', msg)
