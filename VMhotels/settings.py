aad0b7122cb271763eedb025c894aebfbe7ee795
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-10-18 09:16:28 +0200

    Uso delle static per trav.jpg

--- VMhotels/settings.py
+++ VMhotels/settings.py
@@ -112,4 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 LOGIN_URL = '/portal/login/'
