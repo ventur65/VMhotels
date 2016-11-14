3b4432e88bfac3a5e1a14ca389480d6d5bcf2f81
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-11-14 18:49:09 +0100

    GUARDA STO TEST VENTU, DOVREI ESSERCI VICINO SE CAPIAMO STO ERRORE FINISCO TUTTO

--- reservation/tests.py
+++ reservation/tests.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from django.contrib.auth.models import User
 from hotels.models import Room, Hotel
-from django.test import TestCase
-
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
 from .models import Reservation
 from .views import *
+from .forms import ReservationForm
+from django.test.client import Client
 
 def create_reserv (ffdate):
 	"""
@@ -21,13 +23,16 @@ def create_reserv (ffdate):
 class TestBasic(TestCase):
     "Basic tests"
     
-    def setUp(self):		
- 		user = User.objects.create_user(username='prova', email='ciao@mail.it')
-		self.hotel = Hotel.objects.create(user=user)
-		self.room = Room.objects.create(hotel=self.hotel, number=1, cost=1)
-		iidate = datetime.now().date()
-		ffdate = datetime.now().date() + timedelta(days=7)
-		self.res = Reservation.objects.create(user=user, idate=iidate, fdate=ffdate, room=self.room, is_active=True)
+    def setUp(self):	
+    	self.factory = RequestFactory()
+ 	self.user = User.objects.create_user(username='prova', email='ciao@mail.it', password = 'prova')
+	self.hotel = Hotel.objects.create(user=self.user)
+	self.room = Room.objects.create(hotel=self.hotel, number=1, cost=1)
+	iidate = datetime.now().date()
+	ffdate = datetime.now().date() + timedelta(days=7)
+	self.res = Reservation.objects.create(user=self.user, idate=iidate, fdate=ffdate, room=self.room, is_active=True)
+	self.c = Client()
+
     
     def test_res_with_fdate_in_past(self):
     	fdate = self.res.fdate
@@ -41,12 +46,15 @@ class TestBasic(TestCase):
     	instance = Reservation.objects.create(user=uprova, idate=iidate, fdate=ffdate, room=self.room, is_active=True, tel='+393335661378')
     	self.assertTrue(check_res(instance,self.room))
     	
+    def test_add_reservation(self):
+    	self.c.login(username='prova', password='prova')
+    	iidate = datetime.now().date() + timedelta(days=10)
+	ffdate = datetime.now().date() + timedelta(days=18)   	
+	
+	request = self.client.post("include url to post the data given ---->CIAO VENTU QUALE CAZZO E'", {'firstname': 'john', 'lastname': 'prov', 'idate': iidate, 'fdate': ffdate, 'city': 'fabbrico', 'address': 'ciao', 'email': 'prova@gmail.com', 'tel': '+393335661379'})
+	#response = add_reservation(request, self.hotel, self.room)
+	self.assertEqual(request.status_code, 200)
 
-    def test_basic(self):
-        a = 1
-        self.assertEqual(1, a)
-
-    def test_basic_2(self):
-        a = 1
-        assert a == 1
+       
+           	
         	
