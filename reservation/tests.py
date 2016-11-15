26785031d5ae9876543f2c45eae5e2c2e6af7a81
Gianluca Maugeri <gianluca.maugeri27@gmail.com>
2016-11-15 10:54:56 +0100

    messo il gruppo con perm

--- reservation/tests.py
+++ reservation/tests.py
@@ -8,6 +8,9 @@ from .models import Reservation
 from .views import *
 from .forms import ReservationForm
 from django.test.client import Client
+from .urls import *
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 def create_reserv (ffdate):
 	"""
@@ -26,12 +29,22 @@ class TestBasic(TestCase):
     def setUp(self):	
     	self.factory = RequestFactory()
  	self.user = User.objects.create_user(username='prova', email='ciao@mail.it', password = 'prova')
+ 	
+ 	new_group, created = Group.objects.get_or_create(name='owners')
+	ct = ContentType.objects.get_for_model(Reservation)
+	permission = Permission.objects.create(codename='reservation.add_reservation', name='Can add reservation', content_type=ct)
+	new_group.permissions.add(permission)
+	
+	g = Group.objects.get(name='owners') 
+	g.user_set.add(self.user)	
+	
 	self.hotel = Hotel.objects.create(user=self.user)
 	self.room = Room.objects.create(hotel=self.hotel, number=1, cost=1)
 	iidate = datetime.now().date()
 	ffdate = datetime.now().date() + timedelta(days=7)
 	self.res = Reservation.objects.create(user=self.user, idate=iidate, fdate=ffdate, room=self.room, is_active=True, firstname='UEIIII')
 	self.c = Client()
+	self.c.login(username='prova', password='prova')
 
     
     def test_res_with_fdate_in_past(self):
@@ -46,12 +59,11 @@ class TestBasic(TestCase):
     	instance = Reservation.objects.create(user=uprova, idate=iidate, fdate=ffdate, room=self.room, is_active=True, tel='+393335661378')
     	self.assertTrue(check_res(instance,self.room))
     	
-    def test_add_reservation(self):
-    	self.c.login(username='prova', password='prova')
+    def test_add_reservation(self):    	
     	iidate = datetime.now().date() + timedelta(days=10)
 	ffdate = datetime.now().date() + timedelta(days=18)   	
 	
-	request = self.client.post("/reservation/"+str(self.hotel.id)+"/"+str(self.room.id)+"/add/", {'firstname': 'john', 'lastname': 'prov', 'idate': iidate, 'fdate': ffdate, 'city': 'fabbrico', 'address': 'ciao', 'email': 'prova@gmail.com', 'tel': '+393335661379'})
+	request = self.c.post("/reservation/"+str(self.hotel.id)+"/"+str(self.room.id)+"/add/", {'firstname': 'john', 'lastname': 'prov', 'idate': iidate, 'fdate': ffdate, 'city': 'fabbrico', 'address': 'ciao', 'email': 'prova@gmail.com', 'tel': '+393335661379'})
 	
 	print Reservation.objects.get(idate= iidate) #dalla stampa degli oggetti resetvation non ritorna la reservation creata la riga prima con la post
 	
